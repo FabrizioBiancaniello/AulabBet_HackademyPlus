e95b1347a17970df89c876e253194eaa871291fd
FabrizioBiancaniello <fabrizio.biancaniello@aulab.it>
2024-06-12 01:23:21 +0200

    Aggiunto Firebase e Database

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, orderBy, query, onSnapshot, serverTimestamp} from "firebase/firestore";
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD-UcNEr94I0KE5BqhuXQrGw03VU35cUuk",
+  authDomain: "aulabetplus.firebaseapp.com",
+  projectId: "aulabetplus",
+  storageBucket: "aulabetplus.appspot.com",
+  messagingSenderId: "876845694041",
+  appId: "1:876845694041:web:a13b1c1caae37e4acb8548"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+
 function App() {
+  const [bets, setBets] = useState("");
+  const [betPlayerName, setBetPlayerName] = useState("");
+  const [betDescription, setBetDescription] = useState("");
+  const [message, setMessage] = useState("");
+  
+
+  async function getBet(){
+    const colRef = collection(db, "bets");
+    const q = query(colRef, orderBy("created"))
+    onSnapshot(q, (querySnapshot)=>{
+        const items = [];
+        querySnapshot.forEach(doc => {
+            let data = doc.data().created
+            let dataLocale = data.toDate()
+            const options = { year: 'numeric', month: 'numeric', day: 'numeric'};
+            const formattedDate = dataLocale.toLocaleDateString('it-IT', options);
+            items.push({...doc.data(), id: doc.id, created: formattedDate});
+        });
+        setBets(items.reverse())
+    });
+  }
+
+  async function setBet(){
+    if(betPlayerName.length > 5 && betDescription.length > 10){
+      await addDoc(collection(db, "bets"), {
+          name: betPlayerName,
+          description: betDescription,
+          created: serverTimestamp(),
+      });
+      //Reset Campi
+      setBetPlayerName("")
+      setBetDescription("");
+      setMessage({type: "correct", body: "Scommessa Inserita correttamente"})
+      setTimeout(()=>{
+        setMessage("")
+      }, 10000)
+    } else {
+      setMessage({type: "error", body: "Nome o Descrizione non corretti"})
+      setTimeout(()=>{
+        setMessage("")
+      }, 10000)
+    }
+}
+
+
+  useEffect(()=>{
+
+    getBet();
+
+  },[])
 
 
   return (
@@ -30,16 +95,22 @@ function App() {
                       <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                         <div className="accordion-body p-0">
                             <div className="bet-create">
+                              {/* ALERT  */}
+                              {message && 
+                                <div className={`alert fw-bold ${message.type == "error" ? "alert-danger" : "alert-success" }`} role="alert">
+                                  {message.body}
+                                </div>
+                              }
                                 <div className="d-flex flex-column">
-                                    <label className="fs-3 fw-bold text-cus" for="">Inserisci Nome</label>
-                                    <input id="inputName" type="text"/>
+                                    <label className="fs-3 fw-bold text-cus" htmlFor="playerName">Inserisci Nome</label>
+                                    <input className='fs-5 p-2'  onChange={(event)=>setBetPlayerName(event.target.value)} type="text" value={betPlayerName} id='playerName'/>
                                 </div>
                                 <div className="d-flex flex-column">
-                                    <label className="fs-5 mt-3 fw-bold text-cus" for="">Descrizione Scommessa:</label>
-                                    <textarea name="" id="textArea"></textarea>
+                                    <label className="fs-5 mt-3 fw-bold text-cus" htmlFor="description">Descrizione Scommessa:</label>
+                                    <textarea className='p-2' onChange={(event)=>setBetDescription(event.target.value)} value={betDescription} id="description"></textarea>
                                 </div>
                                 <div className="d-flex justify-content-center">
-                                    <button className="btn-custom mt-4 fs-5">AGGIUNGI</button>
+                                    <button className="btn-custom mt-4 fs-5" onClick={setBet}>AGGIUNGI</button>
                                 </div>
                             </div>
                         </div>
@@ -55,7 +126,19 @@ function App() {
                         </div>
                         
                     </div>
-                <div className="row" id="betWrapper">
+                <div className="row" >
+                  {bets && bets.map((bet)=>{
+                    return (
+                      <div className='col-12 col-md-6 col-xl-4 my-2' key={bet.id}>
+                        <div className="bet-box">
+                          <h4 className="text-center mb-5 border-bottom">{bet.name}</h4>
+                          <p className="fw-bold">Descrizione Scommessa:</p>
+                          <p className="p-description">{bet.description}</p>
+                          <p className="text-end fw-bold">{bet.created}</p>
+                        </div>
+                      </div>
+                    )
+                  })}
                     {/* <div className="col-12 col-md-6 col-xl-4">
                         <div className="bet-box">
                             <h4 className="text-center mb-5 border-bottom">Nome Cognome</h4>
