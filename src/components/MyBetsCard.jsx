6d4e65d4eda52d6b950f30d9cbe92e2630fc57ee
FabrizioBiancaniello <fabrizio.biancaniello@aulab.it>
2024-06-28 14:32:21 +0200

    merge

--- src/components/MyBetsCard.jsx
+++ src/components/MyBetsCard.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-// import VisibilitySensor from "react-visibility-sensor";
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import ReactVisibilitySensor from "react-visibility-sensor";
 
@@ -14,15 +13,6 @@ export default function MyBetsCard({ bet, updateVote, utente, notVoted }) {
 
     return (
         <div className='col-10 my-2 p-3'>
-
-            {/* <div className="d-flex justify-content-between mb-3">
-                    <div className="w-75">
-
-                        <h4 className="secondary-title text-uppercase text-truncate fs-5">{bet.name}</h4>
-                    </div>
-                    <p className="mb-0">{bet.created}</p>
-                </div> */}
-
             
                 <div className="row text-white align-items-center justify-content-between p-3 bg-success">
                     <div className="col-8 text-truncate">
@@ -63,27 +53,6 @@ export default function MyBetsCard({ bet, updateVote, utente, notVoted }) {
             {/* <div>
                 <p className="fw-bold text-center secondary-title">Descrizione Scommessa:</p>
                 <p className="p-description">{bet.description}</p>
-            </div> */}
-            {/* {utente && !utente.bets?.includes(bet.id) && notVoted(bet.vote, utente.id) &&
-                <p className="voteBtn m-0" onClick={() => setVoteMenu(true)}>VOTA BET</p>
-            }
-            <div style={{ "height": `${voteMenu ? '100%' : "0%"}`, "opacity": `${voteMenu ? '1' : "0"}` }} className='containerVotes'>
-                <div className="d-flex flex-column justify-content-center p-2 position-relative h-100">
-                    <i className="bi bi-x-circle fs-3 me-2 closeBtn" onClick={() => setVoteMenu(false)}></i>
-                    <div className="text-center">
-                        <h4 className="m-0 mb-3 fs-3 fw-bold">DIFFICOLTA' BET</h4>
-                        <p className="text-uppercase">
-                            Vota la bet in base alla sua difficolta'
-                        </p>
-                    </div>
-                    <div className="d-flex justify-content-evenly ">
-                        <div className='vote-box' onClick={() => { updateVote(bet.id, utente.id, 1); setVoteMenu(false) }}>1</div>
-                        <div className='vote-box' onClick={() => { updateVote(bet.id, utente.id, 2); setVoteMenu(false) }}>2</div>
-                        <div className='vote-box' onClick={() => { updateVote(bet.id, utente.id, 3); setVoteMenu(false) }}>3</div>
-                        <div className='vote-box' onClick={() => { updateVote(bet.id, utente.id, 4); setVoteMenu(false) }}>4</div>
-                        <div className='vote-box' onClick={() => { updateVote(bet.id, utente.id, 5); setVoteMenu(false) }}>5</div>
-                    </div>
-                </div>
             </div> */}
 
         </div>
